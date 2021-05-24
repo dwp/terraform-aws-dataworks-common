ed41cde918d8f298487aad2f95c2a41712e57961
stevenburton <stevenburton@digital.uc.dwp.gov.uk>
2021-05-24 11:30:27 +0100

    Add new required tag

--- locals.tf
+++ locals.tf
@@ -1,26 +1,27 @@
 locals {
-  valid_environments = keys(local.environment_map)
+  valid_environments   = keys(local.environment_map)
   validate_environment = index(local.valid_environments, var.tag_value_environment)
 
   environment_map = {
-    development = "Dev"
-    qa = "Test"
-    integration = "Test"
-    preprod = "Stage"
-    production = "Production"
+    development    = "Dev"
+    qa             = "Test"
+    integration    = "Test"
+    preprod        = "Stage"
+    production     = "Production"
     management-dev = "DT_Tooling"
-    management = "SP_Tooling"
+    management     = "SP_Tooling"
   }
 
   common_tags_map = {
-    Name         = ""
-    Role         = ""
-    Environment  = var.tag_value_environment
-    Application  = "DataWorks"
-    Function     = "Data and Analytics"
-    CreatedBy    = "terraform"
-    Owner        = "DataWorks"
-    Persistence  = "Ignore"
-    AutoShutdown = "False"
+    Name             = ""
+    Role             = ""
+    Environment      = var.tag_value_environment
+    Application      = "DataWorks"
+    Function         = "Data and Analytics"
+    CreatedBy        = "terraform"
+    Owner            = "DataWorks"
+    Persistence      = "Ignore"
+    AutoShutdown     = "False"
+    Business-Project = "PRJ0022507"
   }
 }
